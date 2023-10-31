29b53511c8148413ff59d8bc5a573852390ef8fe
Yanjun Chen (Stella) <146333771+ucfnchb@users.noreply.github.com>
2023-10-31 10:11:44 +0000

    Photoresistors (LDR) + buzzer

---
+++ photosensitive_resistor/photosensitive_resistor.ino
@@ -0,0 +1,24 @@
+int photoPin = A0;
+
+void setup() {
+  //start buzzer
+  Serial.begin(9600);
+  pinMode(11, OUTPUT);
+}
+
+void loop() {
+  ////photosensitive resistor, when the object reach, to get the reads less than 100, results the buzzer works 
+  int light = analogRead(photoPin);
+  Serial.println(light);
+  delay(100);
+  if(light<100)
+  {
+  digitalWrite(11, HIGH);
+  delay(1000);
+  digitalWrite(11, LOW);
+  delay(1000);
+    
+  }
+
+
+}
